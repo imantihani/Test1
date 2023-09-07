97205db59e7506293efbc893d73379574d1cff80
imantihani <imantihani@unisel.edu.my>
2023-09-07 10:49:35 +0800

    update home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('hello world!') }}
                 </div>
             </div>
         </div>
